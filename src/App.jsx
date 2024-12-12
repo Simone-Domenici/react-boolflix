c42d24c94f96da81c118218a08b4c15fa7041f8e
Simone-Domenici <domenici.simone00@gmail.com>
2024-12-12 11:47:48 +0100

    ADD first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import GlobalContext from './contexts/GlobalContext'
+
+function App() {
+ 
+  return (
+      <GlobalContext.Provider>
+      </GlobalContext.Provider>
+  )
+}
+
+export default App
